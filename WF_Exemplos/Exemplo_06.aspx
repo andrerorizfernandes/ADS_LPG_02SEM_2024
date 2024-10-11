21d54fd32b8ad9215e7900b38ef78a61a4bf1239
andremuriae@yahoo.com.br <andrerorizfernandes>
2024-10-10 21:49:20 -0300

    Aula dia 10/10 PT2

---
+++ WF_Exemplos/Exemplo_06.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Exemplo_06.aspx.cs" Inherits="WF_Exemplos.Exemplo_06" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            z-index: 1;
+            left: 10px;
+            top: 56px;
+            position: absolute;
+            height: 226px;
+            width: 936px;
+        }
+    </style>
+</head>
+<body style="height: 195px">
+    <form id="form1" runat="server">               
+        <asp:GridView ID="gvdAluno" runat="server" AutoGenerateColumns="False" OnRowCancelingEdit="gvdUsuarios_RowCancelingEdit" OnRowDeleting="gvdUsuarios_RowDeleting" OnRowEditing="gvdUsuarios_RowEditing" OnRowUpdating="gvdUsuarios_RowUpdating" BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2">
+            <Columns>
+                <asp:TemplateField HeaderText="Código">
+                    <EditItemTemplate>
+                        <asp:Label ID="lblId" runat="server" Text='<%# Bind("Id") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblId" runat="server" Text='<%# Bind("Id") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Nome">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtNome" runat="server" Text='<%# Bind("Nome") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblNome" runat="server" Text='<%# Bind("Nome") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Matrícula">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtMatricula" runat="server" Text='<%# Bind("Matricula") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblMatricula" runat="server" Text='<%# Bind("Matricula") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Curso">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtCurso" runat="server" Text='<%# Bind("Curso") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblCurso" runat="server" Text='<%# Bind("Curso") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField>
+                    <EditItemTemplate>
+                        <asp:Button ID="btnSalvar" runat="server" Text="Salvar" CommandName="Update"></asp:Button>
+                        <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" CommandName="Cancel"></asp:Button>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Button ID="btnEditar" runat="server" Text="Editar" CommandName="Edit"></asp:Button>
+                        <asp:Button ID="btnExcluir" runat="server" Text="Excluir" CommandName="Delete"></asp:Button>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+            </Columns>
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>    
+        <p></p>
+        <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+            <tr>                    
+                <td style="width:300">
+                    Nome:<br/>
+                    <asp:TextBox ID="txtNovoNome" runat="server" Width="300" />
+                </td>
+                <td style="width:120px">
+                    Matricula:<br/>
+                    <asp:TextBox ID="txtNovaMatricula" runat="server" Width="120"/>
+                </td>
+                <td style="width:180px">
+                    Curso:<br/>
+                    <asp:TextBox ID="txtNovoCurso" runat="server" Width="180"/>
+                </td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="Adicionar" OnClick="Insert"></asp:Button>
+                </td>
+            </tr>
+        </table>                 
+    </form>
+</body>
+</html>
---
+++ WF_Exemplos/Exemplo_06.aspx.cs
@@ -0,0 +1,206 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WF_Exemplos
+{
+    public partial class Exemplo_06 : System.Web.UI.Page
+    {
+        private MySqlConnection Conexao()
+        {
+            MySqlConnection con = new MySqlConnection(@"server=127.0.0.1;user=root;password=18071988;database=ads");
+            con.Open();
+
+            return con;
+        }
+
+        private void ExibirDados(List<Aluno> pDados)
+        {
+            gvdAluno.DataSource = pDados;
+            gvdAluno.DataBind();
+        }
+
+        private void LimparCampos()
+        {
+            txtNovoNome.Text = string.Empty;
+            txtNovaMatricula.Text = string.Empty;
+            txtNovoCurso.Text = string.Empty;
+        }
+
+        protected void ExibirMensagem(string pMensagem)
+        {
+            string sJavaScript = "<script language=javascript>\n";
+            sJavaScript += "alert('" + pMensagem + "');";
+            sJavaScript += "\n";
+            sJavaScript += "</script>";
+            ClientScript.RegisterStartupScript(typeof(string), "MessageBox", sJavaScript);
+        }
+
+        private List<Aluno> CarregarAlunos()
+        {
+            MySqlCommand comando = new MySqlCommand("SELECT id, matricula, nome, curso FROM aluno", Conexao());
+            MySqlDataReader dr = comando.ExecuteReader();
+
+            List<Aluno> alunos = new List<Aluno>();
+            while (dr.Read())
+            {
+                Aluno aluno = new Aluno();
+
+                aluno.Id = Convert.ToInt32(dr["id"].ToString());
+                aluno.Nome = dr["nome"].ToString();
+                aluno.Matricula = dr["matricula"].ToString();
+                aluno.Curso = dr["curso"].ToString();
+
+                alunos.Add(aluno);
+            }
+
+            return alunos;
+        }
+
+        private bool InserirAluno(Aluno aluno)
+        {
+            MySqlCommand comando = new MySqlCommand("Insert Into Aluno (matricula, nome, curso) " +
+                "Value (@matricula, @nome, @curso)", Conexao());
+            comando.Parameters.AddWithValue("@matricula", aluno.Matricula);
+            comando.Parameters.AddWithValue("@nome", aluno.Nome);
+            comando.Parameters.AddWithValue("@curso", aluno.Curso);
+            MySqlDataReader dr = comando.ExecuteReader();
+
+            return (dr.RecordsAffected > 0);
+        }
+
+        private bool EditarAluno(Aluno aluno)
+        {
+            MySqlCommand comando = new MySqlCommand("Update Aluno Set matricula=@matricula, nome=@nome, curso=@curso" +
+                " Where id=@id", Conexao());
+            comando.Parameters.AddWithValue("@matricula", aluno.Matricula);
+            comando.Parameters.AddWithValue("@nome", aluno.Nome);
+            comando.Parameters.AddWithValue("@curso", aluno.Curso);
+            comando.Parameters.AddWithValue("@id", aluno.Id.ToString());
+            MySqlDataReader dr = comando.ExecuteReader();
+
+            return (dr.RecordsAffected > 0);
+        }
+
+        private bool ExcluirAluno(Aluno aluno)
+        {
+            MySqlCommand comando = new MySqlCommand("Delete From Aluno" +
+                " Where id=@id", Conexao());
+            comando.Parameters.AddWithValue("@id", aluno.Id.ToString());
+            MySqlDataReader dr = comando.ExecuteReader();
+
+            return (dr.RecordsAffected > 0);
+        }
+
+        private void Inserir()
+        {
+            Aluno a = new Aluno();
+            a.Matricula = txtNovaMatricula.Text;
+            a.Nome = txtNovoNome.Text;
+            a.Curso = txtNovoCurso.Text;
+
+            if (InserirAluno(a))
+            {
+                ExibirDados(CarregarAlunos());
+                ExibirMensagem("Aluno inserido com sucesso");
+                LimparCampos();
+            }
+            else
+            {
+                ExibirMensagem("Erro ao inserir aluno");
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ExibirDados(CarregarAlunos());
+            }
+        }
+
+        protected void Insert(object sender, EventArgs e)
+        {
+            Inserir();
+        }
+
+        protected void gvdUsuarios_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvdAluno.EditIndex = -1;
+            ExibirDados(CarregarAlunos());
+        }
+
+        protected void gvdUsuarios_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int index = e.RowIndex;
+
+            Aluno a = new Aluno();
+            Label lblId = gvdAluno.Rows[index].FindControl("lblId") as Label;
+            if (lblId != null)
+            {
+                a.Id = Convert.ToInt32(lblId.Text);
+            }
+
+            if (ExcluirAluno(a))
+            {
+                ExibirMensagem("Aluno excluído com sucesso");
+            }
+            else
+            {
+                ExibirMensagem("Falha ao excluir aluno");
+            }
+
+            gvdAluno.EditIndex = -1;
+            ExibirDados(CarregarAlunos());
+        }
+
+        protected void gvdUsuarios_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvdAluno.EditIndex = e.NewEditIndex;
+            ExibirDados(CarregarAlunos());
+        }
+
+        protected void gvdUsuarios_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int index = e.RowIndex;
+
+            Aluno a = new Aluno();
+            Label lblId = gvdAluno.Rows[index].FindControl("lblId") as Label;
+            if (lblId != null)
+            {
+                a.Id = Convert.ToInt32(lblId.Text);
+            }
+            TextBox txtNome = gvdAluno.Rows[index].FindControl("txtNome") as TextBox;
+            if (txtNome != null)
+            {
+                a.Nome = txtNome.Text;
+            }
+            TextBox txtMatricula = gvdAluno.Rows[index].FindControl("txtMatricula") as TextBox;
+            if (txtMatricula != null)
+            {
+                a.Matricula = txtMatricula.Text;
+            }
+            TextBox txtCurso = gvdAluno.Rows[index].FindControl("txtCurso") as TextBox;
+            if (txtCurso != null)
+            {
+                a.Curso = txtCurso.Text;
+            }
+
+            if (EditarAluno(a))
+            {
+                ExibirMensagem("Aluno editado com sucesso");
+            }
+            else
+            {
+                ExibirMensagem("Falha ao editar aluno");
+            }
+
+            gvdAluno.EditIndex = -1;
+            ExibirDados(CarregarAlunos());
+        }
+    }
+}
